2a5fdf2ab439fb8245a0a79a9d19d5ef237092c2
17621724700 <17621724700@163.com>
2023-07-19 10:51:59 +0800

    showMainView

--- lib/src/common.dart
+++ lib/src/common.dart
@@ -1,8 +1,23 @@
 
 // import 'pages.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'pages/homePage.dart';
 
 Widget getHomePage(){
   return const HomePage();
+}
+
+showMainView(context){
+
+  showModalBottomSheet(context: context,
+      backgroundColor: Colors.white,
+      isScrollControlled: false,
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.only(topLeft: (Radius.circular(10)),topRight: (Radius.circular(10)))),
+      builder: (BuildContext context){
+        return Container(
+          height: 300,
+          child: Center(child: Text('居中文字'),),
+        );
+      });
 }
