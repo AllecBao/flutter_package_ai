c5a60b601ff001208f71c04d91dc28878840b6aa
17621724700 <17621724700@163.com>
2023-07-19 18:46:37 +0800

    update

--- lib/src/views/homeView.dart
+++ lib/src/views/homeView.dart
@@ -4,11 +4,11 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart';
+// import 'package:permission_handler/permission_handler.dart';
 import '../viewModel/soundViewModel.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'package:flutter_sound/flutter_sound.dart';
-import 'package:flutter_sound_platform_interface/flutter_sound_recorder_platform_interface.dart';
+// import 'package:flutter/foundation.dart' show kIsWeb;
+// import 'package:flutter_sound/flutter_sound.dart';
+// import 'package:flutter_sound_platform_interface/flutter_sound_recorder_platform_interface.dart';
 
 class HomeView extends StatefulWidget{
   const HomeView({Key? key}) : super(key: key);
@@ -19,80 +19,79 @@ class HomeView extends StatefulWidget{
 
 class _HomeViewState extends State<HomeView>{
 
-  Codec _codec = Codec.aacMP4;
-  String _mPath = 'tau_file.mp4';
-  bool recording = false;
-
-  FlutterSoundPlayer? _mPlayer = FlutterSoundPlayer();
-  FlutterSoundRecorder? _mRecorder = FlutterSoundRecorder();
-
-  Future<void> openTheRecorder() async {
-    var status = await Permission.microphone.request();
-    if (status != PermissionStatus.granted) {
-      throw RecordingPermissionException('Microphone permission not granted');
-    }
-    await _mRecorder!.openAudioSession();
-
-    if (!await _mRecorder!.isEncoderSupported(_codec) && kIsWeb) {
-      print('***********kIsWeb');
-      _codec = Codec.opusWebM;
-      _mPath = 'tau_file.webm';
-      if (!await _mRecorder!.isEncoderSupported(_codec) && kIsWeb) {
-        return;
-      }
-    }
-  }
+  // Codec _codec = Codec.aacMP4;
+  // String _mPath = 'tau_file.mp4';
+  // bool recording = false;
+  //
+  // FlutterSoundPlayer? _mPlayer = FlutterSoundPlayer();
+  // FlutterSoundRecorder? _mRecorder = FlutterSoundRecorder();
+  //
+  // Future<void> openTheRecorder() async {
+  //   var status = await Permission.microphone.request();
+  //   if (status != PermissionStatus.granted) {
+  //     throw RecordingPermissionException('Microphone permission not granted');
+  //   }
+  //   await _mRecorder!.openAudioSession();
+  //
+  //   if (!await _mRecorder!.isEncoderSupported(_codec) && kIsWeb) {
+  //     print('***********kIsWeb');
+  //     _codec = Codec.opusWebM;
+  //     _mPath = 'tau_file.webm';
+  //     if (!await _mRecorder!.isEncoderSupported(_codec) && kIsWeb) {
+  //       return;
+  //     }
+  //   }
+  // }
 
   @override
   void initState() {
 
-    _mPlayer!.openAudioSession().then((value) {});
-    openTheRecorder().then((value) {
-      setState(() {});
-    });
+    // _mPlayer!.openAudioSession().then((value) {});
+    // openTheRecorder().then((value) {
+    //   setState(() {});
+    // });
     super.initState();
 
-   Future.delayed(const Duration(milliseconds: 500),(){
-      print('***********delayed');
-      _codec = Codec.pcm16WAV;
-      _mPath = 'https://resource.51ptt.net/ai/temp/tmm_ai_welcome.wav';
-      play();
-    });
+   // Future.delayed(const Duration(milliseconds: 500),(){
+   //    print('***********delayed');
+   //    _codec = Codec.pcm16WAV;
+   //    _mPath = 'https://resource.51ptt.net/ai/temp/tmm_ai_welcome.wav';
+   //    play();
+   //  });
   }
-  void play() {
-    _mPlayer!.startPlayer(
-        fromURI: _mPath,
-        // 'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3',
-        codec: _codec,
-        whenFinished: () {
-          setState(() {});
-        })
-        .then((value) {
-      setState(() {});
-    });
-  }
-
-  void record() async {
-    _codec = Codec.aacMP4;
-    _mPath = 'tau_file.mp4';
-    _mRecorder!.startRecorder(
-      toFile: _mPath,
-      codec: _codec,
-      audioSource: AudioSource.microphone,
-    ).then((value) {});
-
-    setState(() {
-      recording = true;
-    });
-  }
-
-  void stopRecorder() async {
-    await _mRecorder!.stopRecorder().then((value) {
-      setState(() {
-        recording = false;
-      });
-    });
-  }
+  // void play() {
+  //   _mPlayer!.startPlayer(
+  //       fromURI: _mPath,
+  //       codec: _codec,
+  //       whenFinished: () {
+  //         setState(() {});
+  //       })
+  //       .then((value) {
+  //     setState(() {});
+  //   });
+  // }
+  //
+  // void record() async {
+  //   _codec = Codec.aacMP4;
+  //   _mPath = 'tau_file.mp4';
+  //   _mRecorder!.startRecorder(
+  //     toFile: _mPath,
+  //     codec: _codec,
+  //     audioSource: AudioSource.microphone,
+  //   ).then((value) {});
+  //
+  //   setState(() {
+  //     recording = true;
+  //   });
+  // }
+  //
+  // void stopRecorder() async {
+  //   await _mRecorder!.stopRecorder().then((value) {
+  //     setState(() {
+  //       recording = false;
+  //     });
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context){
@@ -105,7 +104,7 @@ class _HomeViewState extends State<HomeView>{
             ),
             ElevatedButton(
                 onPressed: () {
-                  play();
+                  // play();
                 },
                 child: Text('Play')),
             SizedBox(
@@ -114,13 +113,13 @@ class _HomeViewState extends State<HomeView>{
             // 一般播放和录音没啥关系
             ElevatedButton(
                 onPressed: () {
-                  if (recording) {
-                    stopRecorder();
-                  } else {
-                    record();
-                  }
+                  // if (recording) {
+                  //   stopRecorder();
+                  // } else {
+                  //   record();
+                  // }
                 },
-                child: Text(recording ? 'Stop' : 'Record'))
+                child: Text( 'Record'))
           ],
         ),
       ),
@@ -130,9 +129,9 @@ class _HomeViewState extends State<HomeView>{
   @override
   void dispose() {
     print('***********dispose');
-    if(recording){
-      stopRecorder();
-    }
+    // if(recording){
+    //   stopRecorder();
+    // }
     super.dispose();
   }
 }
